073a40d8a7efe77c898d34d8bbcac9a479505b5d
sira nassoko <siranassoko@siras-MacBook-Pro-7.local>
2018-12-09 19:55:53 -0500

    search bar component added

---
+++ src/components/search-bar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {firebaseConnect, isLoaded, isEmpty} from "react-redux-firebase";
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import Suggestions from './suggestions.js'
+
+class Search extends Component {
+  state = {
+    query: '',
+    results: []
+  }
+
+  /*getInfo = () => {
+    axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+      .then(({ data }) => {
+        this.setState({
+          results: data.data
+        })
+      })
+  }*/
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    }, () => {
+      if (this.state.query && this.state.query.length > 1) {
+        if (this.state.query.length % 2 === 0) {
+          //this.getInfo()
+        }
+      } else if (!this.state.query) {
+      }
+    })
+  }
+
+  render() {
+    return (
+      <form className = "searchBar">
+        <input
+          className = 'searchBarInput'
+          placeholder="Search"
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <Suggestions results={this.state.results} />
+      </form>
+    )
+  }
+}
+
+export default Search
